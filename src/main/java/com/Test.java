24355ae2e54ec04b9368fa182c1ea1e690a71dc8
Andrei Pashkevich <Andrei_Pashkevich2@epam.com>
2018-01-26 11:45:46 +0300

    dwwdw

---
+++ src/main/java/com/Test.java
@@ -0,0 +1,15 @@
+package com;
+
+public class Test {
+
+
+  public static void main(String[] args) {
+    Test test = new Test();
+    System.out.println(test.HelloMethod());
+  }
+
+
+  public String HelloMethod() {
+    return "Hello!";
+  }
+}
---
+++ src/test/java/com/TestTest.java
@@ -0,0 +1,15 @@
+package com;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestTest {
+
+  @Test
+  public void helloMethod() {
+    String expected = "Hello";
+    assertEquals(expected, "Hello");
+
+  }
+}
